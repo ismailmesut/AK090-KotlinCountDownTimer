e999873927fd390f25e54fbe468cca7a2062ecd9
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-24 03:09:01 +0300

    updated: count down timer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlincountdowntimer"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlincountdowntimer"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlincountdowntimer/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincountdowntimer/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.ismailmesutmujde.kotlincountdowntimer
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlincountdowntimer.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonStart.setOnClickListener {
+
+            val counter = object : CountDownTimer(30000,1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    bindingMain.textViewOutput.text = "Remaining Time : ${millisUntilFinished/1000} sec"
+                }
+
+                override fun onFinish() {
+                    bindingMain.textViewOutput.text = "Finished!"
+                }
+
+            }
+
+            counter.start()
+
+        }
 
     }
 }
